3daf54047216c4c883271e34ee37517acbce6950
alanjding <47194098+alanjding@users.noreply.github.com>
2019-11-10 00:16:27 -0500

    More testing

--- app.py
+++ app.py
@@ -80,7 +80,8 @@ def login():
 
 @app.route('/exchange-token')
 def check_eligibility():
-    auth_code = request.args.get('code')
+    auth_code = str(request.args.get('code'))
+    print(request.args)
 
     access_details = get_access_details(auth_code)
     access_token = access_details['access_token']
