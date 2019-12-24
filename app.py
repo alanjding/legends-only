e19da4f68c241e9f8197f1e708e57f11db89dc3e
alanjding <47194098+alanjding@users.noreply.github.com>
2019-12-24 12:30:00 -0500

    new version using stravalib, bootstrap. placeholder pages only for the most part rn

--- app.py
+++ app.py
@@ -8,10 +8,9 @@
 from sys import argv
 from flask import Flask, request, make_response, redirect, render_template, \
     url_for
-from flask_heroku import Heroku
+from stravalib import Client
 import os
 import requests
-import re
 
 # ------------------------------------------------------------------------------
 
@@ -19,66 +18,87 @@ API_BASE_URL = 'https://www.strava.com/api/v3'
 LEGENDS_OUT = '22248347'
 LEGENDS_BACK = '22248349'
 
-# ------------------------------------------------------------------------------
-
-# global data
 app = Flask(__name__, template_folder='./templates')
-is_authorized = False
 
 # ------------------------------------------------------------------------------
 
-# helper function definition
-def get_access_details(auth_code):
-    url = 'https://www.strava.com/oauth/token' + \
-          '?client_id=' + os.environ['CLIENT_ID'] + \
-          '&client_secret=' + os.environ['CLIENT_SECRET'] + \
-          '&code=' + auth_code + \
-          '&grant_type=authorization_code'
-
-    print(url)
-    r = requests.post(url)
-
-    return r.json()
-
-# ------------------------------------------------------------------------------
-
-# site pages
+@app.before_request
+def before_request():
+    # redirect http to https
+    if request.url[0:5] != 'https':
+        return redirect(request.url.replace('http', 'https', 1))
 
 @app.route('/login')
 def login():
-    html = render_template('login.html')
-    return make_response(html)
+    return make_response(render_template('login.html'))
 
-@app.route('/exchange-token')
-def check_eligibility():
-    global is_authorized
+# @app.route('/exchange-token')
+# def check_eligibility():
+#
+#     # incredibly hacky workaround for inconsistent Strava response
+#     url_str = str(request.query_string)
+#     print('*********url_str**********' + url_str)
+#
+#     # try again if no code returned
+#     if 'code' not in url_str:
+#         return redirect('https://www.strava.com/oauth/authorize' +
+#                         '?client_id=32278&response_type=code' +
+#                         '&redirect_uri=http://legends-only.herokuapp.com/' +
+#                         'exchange-token&approval_prompt=force&scope=read')
+#
+#     match = re.search(r'[0-9a-f]{40}[0-9a-f]*?', url_str)
+#     print(match)
+#
+#     if match is None:
+#         return redirect(url_for('login'))
+#
+#     auth_code = match.group(0)
+#     print('*********auth_code**********' + auth_code)
+#
+#     access_details = helper.get_access_details(auth_code)
+#     print('*********access_details**********' + str(access_details))
+#     access_token = access_details['access_token']
+#     user_id = access_details['athlete']['id']
+#     username = access_details['athlete']['firstname'] + ' ' + \
+#         access_details['athlete']['lastname']
+#
+#     r_out = requests.get(API_BASE_URL +
+#                          '/segments/' + LEGENDS_OUT + '/all_efforts' +
+#                          '?access_token=' + access_token)
+#
+#     legends_out_data = r_out.json()
+#
+#     r_back = requests.get(API_BASE_URL +
+#                           '/segments/' + LEGENDS_BACK + '/all_efforts' +
+#                           '?access_token=' + access_token)
+#
+#     legends_back_data = r_back.json()
+#
+#     if len(legends_out_data) == 0 or \
+#        len(legends_back_data) == 0 or \
+#        username == 'Adam Chang':
+#         return redirect(url_for('not-legend'))
+#     else:
+#         is_authorized = True
+#         return redirect(url_for('index'))
 
-    # incredibly hacky workaround for inconsistent Strava response
-    url_str = str(request.query_string)
-    print('*********url_str**********' + url_str)
+@app.route('/authorization')
+def authorization():
+    code = request.args.get('code')
+    client = Client()
+    token_response = client.exchange_code_for_token(
+        client_id=os.environ.get('CLIENT_ID'),
+        client_secret=os.environ.get('CLIENT_SECRET'),
+        code=code
+    )
 
-    # try again if no code returned
-    if 'code' not in url_str:
-        return redirect('https://www.strava.com/oauth/authorize' +
-                        '?client_id=32278&response_type=code' +
-                        '&redirect_uri=http://legends-only.herokuapp.com/' +
-                        'exchange-token&approval_prompt=force&scope=read')
+    access_token = token_response['access_token']
 
-    match = re.search(r'[0-9a-f]{40}[0-9a-f]*?', url_str)
-    print(match)
+    client = Client(access_token=access_token)
 
-    if match is None:
-        return redirect(url_for('login'))
-
-    auth_code = match.group(0)
-    print('*********auth_code**********' + auth_code)
-
-    access_details = get_access_details(auth_code)
-    print('*********access_details**********' + str(access_details))
-    access_token = access_details['access_token']
-    user_id = access_details['athlete']['id']
-    username = access_details['athlete']['firstname'] + ' ' + \
-        access_details['athlete']['lastname']
+    athlete = client.get_athlete()
+    if athlete.firstname == 'Adam' and athlete.lastname == 'Chang':
+        return redirect(url_for('not_legend'))
 
     r_out = requests.get(API_BASE_URL +
                          '/segments/' + LEGENDS_OUT + '/all_efforts' +
@@ -92,27 +112,25 @@ def check_eligibility():
 
     legends_back_data = r_back.json()
 
-    if len(legends_out_data) == 0 or \
-       len(legends_back_data) == 0 or \
-       username == 'Adam Chang':
-        return redirect(url_for('not-legend'))
-    else:
-        is_authorized = True
-        return redirect(url_for('index'))
+    if len(legends_out_data) == 0 or len(legends_back_data) == 0:
+        return redirect(url_for('not_legend'))
+
+    # user is authorized at this point
+    response = redirect(url_for('chat'))
+    response.set_cookie('authcookie', os.environ.get('VERIFICATION_KEY'))
+    return response
 
 @app.route('/not-legend')
 def not_legend():
     html = render_template('not-eligible.html')
     return make_response(html)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    if is_authorized:
-        html = render_template('index.html')
-        return make_response(html)
-    else:
+@app.route('/chat')
+def chat():
+    if request.cookies.get('authcookie') != os.environ.get('VERIFICATION_KEY'):
         return redirect(url_for('login'))
+
+    return make_response(render_template('chat.html'))
 
 # ------------------------------------------------------------------------------
 
