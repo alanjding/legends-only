9aa0f04a69d3313f833d489ce34587447668d561
alanjding <47194098+alanjding@users.noreply.github.com>
2019-12-24 14:22:37 -0500

    a few more interface improvements

--- app.py
+++ app.py
@@ -32,55 +32,14 @@ def before_request():
 def login():
     return make_response(render_template('login.html'))
 
-# @app.route('/exchange-token')
-# def check_eligibility():
-#
-#     # incredibly hacky workaround for inconsistent Strava response
-#     url_str = str(request.query_string)
-#     print('*********url_str**********' + url_str)
-#
-#     # try again if no code returned
-#     if 'code' not in url_str:
-#         return redirect('https://www.strava.com/oauth/authorize' +
-#                         '?client_id=32278&response_type=code' +
-#                         '&redirect_uri=http://legends-only.herokuapp.com/' +
-#                         'exchange-token&approval_prompt=force&scope=read')
-#
-#     match = re.search(r'[0-9a-f]{40}[0-9a-f]*?', url_str)
-#     print(match)
-#
-#     if match is None:
-#         return redirect(url_for('login'))
-#
-#     auth_code = match.group(0)
-#     print('*********auth_code**********' + auth_code)
-#
-#     access_details = helper.get_access_details(auth_code)
-#     print('*********access_details**********' + str(access_details))
-#     access_token = access_details['access_token']
-#     user_id = access_details['athlete']['id']
-#     username = access_details['athlete']['firstname'] + ' ' + \
-#         access_details['athlete']['lastname']
-#
-#     r_out = requests.get(API_BASE_URL +
-#                          '/segments/' + LEGENDS_OUT + '/all_efforts' +
-#                          '?access_token=' + access_token)
-#
-#     legends_out_data = r_out.json()
-#
-#     r_back = requests.get(API_BASE_URL +
-#                           '/segments/' + LEGENDS_BACK + '/all_efforts' +
-#                           '?access_token=' + access_token)
-#
-#     legends_back_data = r_back.json()
-#
-#     if len(legends_out_data) == 0 or \
-#        len(legends_back_data) == 0 or \
-#        username == 'Adam Chang':
-#         return redirect(url_for('not-legend'))
-#     else:
-#         is_authorized = True
-#         return redirect(url_for('index'))
+@app.route('/logout')
+def logout():
+    response = redirect(url_for('login'))
+
+    # delete authorization cookie
+    response.set_cookie('authcookie', '', expires=0)
+
+    return response
 
 @app.route('/authorization')
 def authorization():
@@ -97,7 +56,8 @@ def authorization():
     client = Client(access_token=access_token)
 
     athlete = client.get_athlete()
-    if athlete.firstname == 'Adam' and athlete.lastname == 'Chang':
+    name = athlete.firstname + ' ' + athlete.lastname
+    if name == 'Adam Chang':
         return redirect(url_for('not_legend'))
 
     r_out = requests.get(API_BASE_URL +
@@ -116,7 +76,7 @@ def authorization():
         return redirect(url_for('not_legend'))
 
     # user is authorized at this point
-    response = redirect(url_for('chat'))
+    response = redirect(url_for('chat', name=name))
     response.set_cookie('authcookie', os.environ.get('VERIFICATION_KEY'))
     return response
 
@@ -131,7 +91,9 @@ def chat():
     if request.cookies.get('authcookie') != os.environ.get('VERIFICATION_KEY'):
         return redirect(url_for('login'))
 
-    return make_response(render_template('chat.html'))
+    name = request.args.get('name')
+
+    return make_response(render_template('chat.html', name=name))
 
 # ------------------------------------------------------------------------------
 
